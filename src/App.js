b3fa0412b67bedfc0d57a77d5a7b4b2edb612d2c
Ramez <rameznoshy@gmail.com>
2021-11-29 00:22:04 +0200

    feat: complete courses page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-import CourseCard from './components/courseCard/CourseCard';
 import CourseCardContainer from './components/courseCard/CourseCardContainer';
+import PageHOC from './components/pageHOC/PageHOC';
+import styles from './components/Common.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <CourseCardContainer />
-        <CourseCard courseName="test" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React : D
-        </a>
-      </header>
-    </div>
+    <PageHOC>
+      <div className={styles.title}>Courses</div>
+      <CourseCardContainer />
+    </PageHOC>
   );
 }
 
