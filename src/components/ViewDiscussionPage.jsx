c4d60e5b758663f2aa0c290320e7d26605ad4908
Ramez <rameznoshy@gmail.com>
2021-11-29 02:15:21 +0200

    feat: use pageHOC in discussions page

--- src/components/ViewDiscussionPage.jsx
+++ src/components/ViewDiscussionPage.jsx
@@ -3,17 +3,21 @@ import PropTypes from 'prop-types';
 import { useParams } from 'react-router-dom';
 import { useGetDiscussion } from '../query';
 import { CircularProgress } from '@mui/material';
+import PageHOC from './pageHOC/PageHOC';
+import styles from './Common.module.css';
 
 const ViewDiscussionPage = (props) => {
   const { discussionId } = useParams();
   const { isLoading, data } = useGetDiscussion(discussionId);
-  return isLoading ? (
-    <CircularProgress />
-  ) : (
-    <div>
+  if (isLoading) {
+    return <CircularProgress />;
+  }
+  return (
+    <PageHOC>
+      <div className={styles.title}>Discussions</div>
       discuss ur life choices. discussionId: {discussionId}. Data:{' '}
       {JSON.stringify(data)}
-    </div>
+    </PageHOC>
   );
 };
 
