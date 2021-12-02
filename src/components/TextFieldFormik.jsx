9a0b6545e15be9374cd3c119e75dcb3cf1dfd99c
Ramez <rameznoshy@gmail.com>
2021-12-02 13:40:05 +0200

    feat: formik textfield

---
+++ src/components/TextFieldFormik.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { TextField } from '@mui/material';
+
+const TextFieldFormik = ({ formik, formikKey, ...props }) => {
+  return (
+    <TextField
+      id={formikKey}
+      value={formik.values[formikKey]}
+      error={formik.touched[formikKey] && Boolean(formik.errors[formikKey])}
+      helperText={formik.touched[formikKey] && formik.errors[formikKey]}
+      {...formik.getFieldProps(formikKey)}
+      {...props}
+    />
+  );
+};
+
+TextFieldFormik.propTypes = {};
+
+export default TextFieldFormik;
