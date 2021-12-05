bad3f9c5cd492b2c8a0b3becd5cf70ec3079f34e
DigitalPhoenixX <MohamedSaidSallam@outlook.com>
2021-12-05 17:32:48 +0200

    refactor: replace nested inline if with if return

--- src/components/CoursePage.jsx
+++ src/components/CoursePage.jsx
@@ -6,22 +6,27 @@ import PageHOC from './pageHOC/PageHOC';
 import commonStyles from './Common.module.css';
 import DiscussionCardContainer from './discussionCard/DiscussionCardContainer';
 import CreateDiscussion from './createDiscussion/CreateDiscussion';
+import ErrorOccurred from './ErrorOccurred';
 
 const CoursePage = () => {
   const { courseId } = useParams();
   const { isLoading, data, isError } = useGetDiscussions(courseId);
   const [isCreateVisible, setIsCreateVisible] = useState(false);
 
+  let child;
+
+  if (isLoading) {
+    child = <CircularProgress />;
+  } else if (isError) {
+    child = <ErrorOccurred />;
+  }
+
   return (
     <PageHOC>
-      {isLoading ? (
-        <CircularProgress />
-      ) : isError ? (
-        <div>An unkown error has occured</div>
-      ) : (
+      {child || (
         <>
           <div className={`${commonStyles.title} ${commonStyles.flexRow}`}>
-            <div> {data.courseName}</div>
+            <div>{data.courseName}</div>
             {!isCreateVisible && (
               <Button
                 variant="contained"
