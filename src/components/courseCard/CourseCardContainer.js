b11d879015a9d7a912409050da1acc400fc5e2a5
DigitalPhoenixX <MohamedSaidSallam@outlook.com>
2021-12-05 17:09:02 +0200

    refactor: remove unnecessary variables

--- src/components/courseCard/CourseCardContainer.js
+++ src/components/courseCard/CourseCardContainer.js
@@ -7,10 +7,8 @@ import routes from '../../constants/routes';
 import { CircularProgress } from '@mui/material';
 
 const CourseCardContainer = () => {
-  const { data, isLoading, isError } = useGetCourses();
   const navigate = useNavigate();
-
-  const onCourseClick = (courseId) => navigate(`${routes.Courses}${courseId}`);
+  const { data, isLoading, isError } = useGetCourses();
 
   if (isLoading) {
     return (
@@ -18,21 +16,25 @@ const CourseCardContainer = () => {
         <CircularProgress />
       </div>
     );
-  } else if (isError) {
-    return <div>An unkown error has occured</div>;
-  } else if (data.length === 0) {
+  }
+  if (isError) {
+    return <div>An unknown error has occurred</div>;
+  }
+  if (data.length === 0) {
     return <div>No courses were found</div>;
   }
 
-  const coursesJSX = data.map(({ courseName, id }) => (
-    <CourseCard
-      courseName={courseName}
-      key={id}
-      onClick={() => onCourseClick(id)}
-    />
-  ));
-
-  return <div className={styles.cardsContainer}>{coursesJSX}</div>;
+  return (
+    <div className={styles.cardsContainer}>
+      {data.map(({ courseName, id }) => (
+        <CourseCard
+          courseName={courseName}
+          key={id}
+          onClick={() => navigate(`${routes.Courses}${id}`)}
+        />
+      ))}
+    </div>
+  );
 };
 
 CourseCardContainer.propTypes = {};
